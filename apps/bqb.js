912e02cd42cc23bb2522ac6a3653403856919985
庆 <bxianxo@qq.com>
2025-02-17 12:32:48 +0800

    Update bqb.js
    修复没用的东西
    
    Signed-off-by: 庆 <bxianxo@qq.com>

--- apps/bqb.js
+++ apps/bqb.js
@@ -52,21 +52,22 @@ export class bqb extends plugin {
     if (type === '甘城' || type === '猫羽雫' || type === '甘城猫猫') {
       let data = await fs.readFileSync(`${PluginPath}/config/AllAPI.json`)
       const API = JSON.parse(data)
-      let api = API.api6.url + '?name=甘城猫猫'
+      let api = API.api5.url + '?name=甘城猫猫'
       await e.reply(segment.image(api))
       return true
     }
     if (!type) {
       let data = await fs.readFileSync(`${PluginPath}/config/AllAPI.json`)
       const API = JSON.parse(data)
-      let api = API.api6.url
+      let api = API.api22.url
       await e.reply(segment.image(api))
       return true
     }
     let data = await fs.readFileSync(`${PluginPath}/config/AllAPI.json`)
     const API = JSON.parse(data)
-    let api = API.api6.url + `?name=${type}`
+    let api = API.api5.url + `?name=${type}`
     await e.reply(segment.image(api))
     return true
   }
 }
+
